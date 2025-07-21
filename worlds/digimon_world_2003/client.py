989bbe676f12b9d6c595faa6f842ac61ef75450f
markisha64 <marko.grizelj03@gmail.com>
2025-07-21 21:47:06 +0200

    chore: more things

--- worlds/digimon_world_2003/client.py
+++ worlds/digimon_world_2003/client.py
@@ -15,14 +15,12 @@ class DMW2003Client(BizHawkClient):
 
     async def validate_rom(self, ctx: "BizHawkClientContext") -> bool:
         try:
-            # Check ROM name/patch version
             rom_name = ((await bizhawk.read(ctx.bizhawk_ctx, [(0x100, 6, "ROM")]))[0]).decode("ascii")
             if rom_name != "SLES_039.36":
-                return False  # Not a MYGAME ROM
+                return False
         except bizhawk.RequestFailedError:
-            return False  # Not able to get a response, say no for now
+            return False 
 
-        # This is a MYGAME ROM
         ctx.game = self.game
         ctx.items_handling = 0b001
         ctx.want_slot_data = True
@@ -31,25 +29,13 @@ class DMW2003Client(BizHawkClient):
 
     async def game_watcher(self, ctx: "BizHawkClientContext") -> None:
         try:
-            # Read save data
-            save_data = (await bizhawk.read(
+            inventory = (await bizhawk.read(
                 ctx.bizhawk_ctx,
-                [(0x3000100, 20, "System Bus")]
+                [(0x80048db0, 403, "ram")]
             ))[0]
 
-            # Check locations
-            if save_data[2] & 0x04:
-                await ctx.send_msgs([{
-                    "cmd": "LocationChecks",
-                    "locations": [23]
-                }])
+            print(inventory)
 
-            # Send game clear
-            if not ctx.finished_game and (save_data[5] & 0x01):
-                await ctx.send_msgs([{
-                    "cmd": "StatusUpdate",
-                    "status": ClientStatus.CLIENT_GOAL
-                }])
 
         except bizhawk.RequestFailedError:
             # The connector didn't respond. Exit handler and return to main loop to reconnect
