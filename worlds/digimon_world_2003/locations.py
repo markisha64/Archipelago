5a062f02c19a656f085a2de19b5f2f5f454033f1
markisha64 <marko.grizelj03@gmail.com>
2025-07-24 01:11:29 +0200

    fix: typo

--- worlds/digimon_world_2003/locations.py
+++ worlds/digimon_world_2003/locations.py
@@ -3,11 +3,11 @@ from .items import ItemData,ALL_ITEMS_TABLE
 from typing import Dict
 from BaseClasses import Location,ItemClassification
 
-ALL_LOCATION_TABLE: Dict[str, ItemData] = {k: v for k, v in ALL_ITEMS_TABLE.items()}
+ALL_LOCATIONS_TABLE: Dict[str, ItemData] = {k: v for k, v in ALL_ITEMS_TABLE.items()}
 
-ALL_LOCATION_TABLE["Beat Galacticmon"] = ItemData(6464, ItemClassification.progression)
+ALL_LOCATIONS_TABLE["Beat Galacticmon"] = ItemData(6464, ItemClassification.progression)
 
 def get_location(name: str, player: int):
-    location = ALL_LOCATION_TABLE[name]
+    location = ALL_LOCATIONS_TABLE[name]
     
     return Location(player, name, location.id)
