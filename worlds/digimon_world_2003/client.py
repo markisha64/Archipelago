dfdd46e919bfb1a6222608281e4827f12acded6f
markisha64 <marko.grizelj03@gmail.com>
2025-07-24 23:13:47 +0200

    feat: more client logic

--- worlds/digimon_world_2003/client.py
+++ worlds/digimon_world_2003/client.py
@@ -15,8 +15,8 @@ class DMW2003Client(BizHawkClient):
 
     async def validate_rom(self, ctx: "BizHawkClientContext") -> bool:
         try:
-            rom_name = ((await bizhawk.read(ctx.bizhawk_ctx, [(0x100, 6, "ROM")]))[0]).decode("ascii")
-            if rom_name != "SLES_039.36":
+            rom_name = ((await bizhawk.read(ctx.bizhawk_ctx, [(0x100fc, 20, "MainRAM")]))[0]).decode("ascii")
+            if rom_name != "BESLES-03936DMW3-EUR":
                 return False
         except bizhawk.RequestFailedError:
             return False 
@@ -24,16 +24,49 @@ class DMW2003Client(BizHawkClient):
         ctx.game = self.game
         ctx.items_handling = 0b001
         ctx.want_slot_data = True
+        self.last_timestamp = 0
+        self.expected_inventory = [0 for _ in range(403)]
 
         return True
 
+    async def get_timestamp(self, ctx: "BizHawkClientContext") -> int:
+        clock_bytes = (await bizhawk.read(
+            ctx.bizhawk_ctx,
+            [(0x48d80, 6, "MainRAM")]
+        ))
+
+        hours = int.from_bytes(clock_bytes[0:2], "little")
+        minutes = int.from_bytes(clock_bytes[2:4], "little")
+        seconds = int.from_bytes(clock_bytes[4:6], "little")
+
+        return hours * 3600 + minutes * 60 + seconds
+
     async def game_watcher(self, ctx: "BizHawkClientContext") -> None:
         try:
+            timestamp = await self.get_timestamp(ctx)
+            
             inventory = (await bizhawk.read(
                 ctx.bizhawk_ctx,
-                [(0x80048db0, 403, "ram")]
-            ))[0]
+                [(0x48db0, 403, "MainRAM")]
+            ))
 
+            if timestamp <= self.last_timestamp: 
+                # reloaded save?
+                pass
+            else:
+                # check whether you gained new items
+                self.last_timestamp = timestamp
+
+                to_remove = []
+
+                for i in range(2, 403):
+                    if inventory[i] > self.expected_inventory[i]:
+                        to_remove.append((i, self.expected_inventory[i]))
+                    else:
+                        self.expected_inventory[i] = inventory[i]
+                
+                pass
+            
             print(inventory)
 
 
