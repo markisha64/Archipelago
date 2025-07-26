63e4d6a89e21133274347a72f1b6eddbb43d5d7c
markisha64 <marko.grizelj03@gmail.com>
2025-07-26 23:43:36 +0200

    fix: signature

--- worlds/digimon_world_2003/rules.py
+++ worlds/digimon_world_2003/rules.py
@@ -5,6 +5,6 @@ from BaseClasses import CollectionState
 def items_owned_rule(items: [str], player: int) -> Callable[[CollectionState, int], bool]:
     return lambda state : state.has_all(items, player)
 
-def items_owned_rule_gen(player: int) -> Callable[Callable[[CollectionState, int], bool]]:
+def items_owned_rule_gen(player: int) -> Callable[Callable[[CollectionState], int], bool]:
     return lambda items : items_owned_rule(items, player)
 
